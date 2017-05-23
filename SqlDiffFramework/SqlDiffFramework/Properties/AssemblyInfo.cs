f4949cb539f1d3d59e5b1063712b866567e9ccb4
msorens <msorens@users.noreply.github.com>
2017-05-23 14:04:57 -0700

    Update copyright

--- SqlDiffFramework/SqlDiffFramework/Properties/AssemblyInfo.cs
+++ SqlDiffFramework/SqlDiffFramework/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -10,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CleanCode")]
 [assembly: AssemblyProduct("SqlDiffFramework")]
-[assembly: AssemblyCopyright("Copyright © 2009-2013 Michael Sorens")]
+[assembly: AssemblyCopyright("Copyright © 2009-2017 Michael Sorens")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
